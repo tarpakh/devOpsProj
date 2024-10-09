5a72462a1b43cc9f11d3de9a2633077af3332800
unknown <tarunpakhare11@gmail.com>
2024-10-09 23:39:55 +0530

    Adding project files

---
+++ newApplicationCRUD/newApplicationCRUD/Data/DbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using newApplicationCRUD.Models;
+using System.Linq;
+namespace newApplicationCRUD.Data
+{
+    public class DbInitializer
+    {
+
+        public static void Initialize(ProductContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any products.
+            if (context.Products.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var products = new Products[]
+            {
+                new Products { Name = "Apple", Price = 40, Quantity = 10 },
+                new Products { Name = "Mango", Price = 50, Quantity = 20 },
+            };
+
+            foreach (var p in products)
+            {
+                context.Products.Add(p);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
